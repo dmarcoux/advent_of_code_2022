1e508b6130ac5bd57e64c65ba3efd75ffb69950e
Dany Marcoux <dmarcoux@posteo.de>
2022-12-04 22:28:53 +0100

    Format AdventOfCode2022.Day1

--- lib/advent_of_code2022/day1.ex
+++ lib/advent_of_code2022/day1.ex
@@ -16,16 +16,21 @@ defmodule AdventOfCode2022.Day1 do
 
   defp elves do
     "lib/advent_of_code2022/day1-input.txt"
-    |> File.stream!() # Read the input file line by line
-    |> Enum.map(&String.trim/1) # Remove all leading and trailing whitespaces
-    |> Enum.chunk_while([], &chunk_func/2, &after_func/1) # Create a list of nested lists containing the calories of every elf
-    |> Enum.map(&Enum.sum/1) # Calculate the sum of calories for every elf
-    |> Enum.sort(:desc) # Sort the elves based on their calories total
+    # Read the input file line by line
+    |> File.stream!()
+    # Remove all leading and trailing whitespaces
+    |> Enum.map(&String.trim/1)
+    # Create a list of nested lists containing the calories of every elf
+    |> Enum.chunk_while([], &chunk_func/2, &after_func/1)
+    # Calculate the sum of calories for every elf
+    |> Enum.map(&Enum.sum/1)
+    # Sort the elves based on their calories total
+    |> Enum.sort(:desc)
   end
 
-  defp chunk_func(_separator = "", accumulator), do: { :cont, accumulator, [] }
-  defp chunk_func(calories, accumulator), do: { :cont, [ String.to_integer(calories) | accumulator ] }
+  defp chunk_func(_separator = "", accumulator), do: {:cont, accumulator, []}
+  defp chunk_func(calories, accumulator), do: {:cont, [String.to_integer(calories) | accumulator]}
 
-  defp after_func([]), do: { :cont, [] }
-  defp after_func(accumulator), do: { :cont, accumulator, [] }
+  defp after_func([]), do: {:cont, []}
+  defp after_func(accumulator), do: {:cont, accumulator, []}
 end
