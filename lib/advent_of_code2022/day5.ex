79102d1ccb7d3c95c2cb2764d67941826bcc5522
Dany Marcoux <dmarcoux@posteo.de>
2022-12-07 23:13:32 +0100

    Prepare day 5

---
+++ lib/advent_of_code2022/day5.ex
@@ -0,0 +1,12 @@
+defmodule AdventOfCode2022.Day5 do
+  @moduledoc """
+  https://adventofcode.com/2022/day/5
+  """
+
+  @doc """
+  Find out which crate ends up on top of each stack
+  """
+  def part_1(input) do
+    input
+  end
+end
---
+++ test/advent_of_code2022/day5_test.exs
@@ -0,0 +1,25 @@
+defmodule AdventOfCode2022.Day5Test do
+  use ExUnit.Case
+  doctest AdventOfCode2022.Day5
+
+  test "solves the puzzle" do
+    example = """
+        [D]
+    [N] [C]
+    [Z] [M] [P]
+     1   2   3
+
+    move 1 from 2 to 1
+    move 3 from 1 to 3
+    move 2 from 2 to 1
+    move 1 from 1 to 2
+    """
+
+    input = File.read!("lib/advent_of_code2022/day5-input.txt")
+
+    # assert AdventOfCode2022.Day5.part_1(example) == "CMZ"
+    # assert AdventOfCode2022.Day5.part_1(input) == 305
+    # assert AdventOfCode2022.Day5.part_2(example) == 4
+    # assert AdventOfCode2022.Day5.part_2(input) == 811
+  end
+end
