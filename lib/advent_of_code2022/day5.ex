b1c962cd3abbe8bdafa1d1c02973798559f639ef
Dany Marcoux <dmarcoux@posteo.de>
2022-12-09 20:41:17 +0100

    Reorganize day 5

--- lib/advent_of_code2022/day5.ex
+++ lib/advent_of_code2022/day5.ex
@@ -7,44 +7,70 @@ defmodule AdventOfCode2022.Day5 do
   Find out which crate ends up on top of each stack
   """
   def part_1(input) do
-    input
+    {stack_identifiers, stack_rows} = stack_identifiers_and_rows(input)
+
+    stacks(stack_identifiers)
     |> IO.inspect()
 
-    stacks_of_crates(input)
+    crates(stack_rows)
+    |> IO.inspect()
+
+    # TODO: Figure out how to put crates into stacks
+    # # Add the crates to their respective stack, but this doesn't work since it's not changing `stacks` (obviously, this is funcitonal programming...)
+    # crates
+    # |> Enum.with_index()
+    # |> Enum.each(fn {index, crate} ->
+    #   # TODO: skip if crate is an empty string
+    #   Map.get_and_update(stacks, :"#{index}", fn stack_crates ->
+    #     {stack_crates, [crate | stack_crates]}
+    #   end)
+    # end)
 
     move_instructions(input)
     |> IO.inspect()
   end
 
   # Retrieve stacks of crates from a puzzle input
-  defp stacks_of_crates(puzzle_input) do
+  defp stack_identifiers_and_rows(puzzle_input) do
     # Remove from the puzzle input everything but the stacks of crates string
-    {[stack_identifiers], stacks} =
+    {[stack_identifiers], stack_rows} =
       String.split(puzzle_input, "\n\n")
       |> List.first()
       # Create a list of lines from the stacks of crates string
       |> String.split("\n")
       # Reverse the list of lines to parse the stack of crates from bottom to top
       |> Enum.reverse()
-      # Split the list of lines into two lists, one with the stack identifiers and the other with the stacks of crates
+      # Split the list of lines into two lists, one with the stack identifiers and the other with the rows of crates forming the stacks
       |> Enum.split(1)
 
-    IO.inspect(stacks)
-    IO.inspect(stack_identifiers)
+    # FIXME: There must be a better way to do this, repeating almost the exact same tuple only to get rid of the list for stack_identifiers
+    {stack_identifiers, stack_rows}
+  end
 
-    # Create a map containing the stacks, each key-value pair representing a stack of crates.
-    # The key is the stack identifier while the value is a list of crates in the stack.
+  # Create a map containing the stacks, each key-value pair representing a stack of crates.
+  # The key is the stack identifier while the value is an empty list of crates in the stack.
+  defp stacks(stack_identifiers) do
     String.split(stack_identifiers, " ", trim: true)
     |> Map.new(fn stack_identifier -> {stack_identifier, []} end)
+  end
 
-    # pseudo code
-    #
-    # add crates to their respective stack starting with the bottom crates -> Probably with Regex
+  # Create a list containing a nested list of crates
+  defp crates(stack_rows) do
+    Enum.map(stack_rows, fn stack_row ->
+      # Every stack row might have a crate on every fourth character starting from the second character
+      #
+      # Example of a stack row: "[A] [B] [C]", "    [E] [F]" or "    [B]    "
+      #   Shorter or longer strings with different combinations are possible.
+      String.graphemes(stack_row)
+      # The first character is either a space or `[`, so it can be dropped before the crates are taken
+      |> Enum.drop(1)
+      |> Enum.take_every(4)
+    end)
   end
 
   # Retrieve move instructions from a puzzle input
   #
-  # Example output: [%{"destination" => "1", "move_count" => "1", "origin" => "2"}, %{"destination" => "3", "move_count" => "3", "origin" => "1"}]
+  # Example output: [%{"destination" => "1", "move_count" => "1", "origin" => "2"}]
   defp move_instructions(puzzle_input) do
     # Remove from the puzzle input everything but the move instructions string
     String.split(puzzle_input, "\n\n")
